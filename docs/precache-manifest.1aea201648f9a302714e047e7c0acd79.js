5ef027684e5ab27fae13bbee0dac187e6d4117f0
Alexandre Canuto <alexandrecanuto@AirdeAlexandre.Home>
2021-11-23 12:58:57 +0000

    build(build): changed build folder to deploy in GitHub Pages

---
+++ docs/precache-manifest.1aea201648f9a302714e047e7c0acd79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be644f675a0884cf0f87e09e7fc31d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "914b74280bced386f250",
+    "url": "/static/js/2.b298cc26.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b298cc26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d065dd2a629ef4897c9",
+    "url": "/static/js/main.7217bcab.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/netflix.6dbba458.svg"
+  }
+]);
